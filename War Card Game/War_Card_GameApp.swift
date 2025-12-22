1622edc8d44dcd37209795905f1dfd3d57f7f054
Vlad <vmiloser@andrew.cmu.edu>
2025-12-21 22:56:02 -0500

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Vlad Miloserdov on 12/21/25.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
